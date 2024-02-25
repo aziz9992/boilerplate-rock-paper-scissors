255ed7fe9d3de104b801433f15503ba3d409b651
mouhamed aziz souissi <souissimouhamedaziz99@gmail.com>
2024-02-25 20:36:02 +0000

    ex1 done

--- test_module.py
+++ test_module.py
@@ -15,7 +15,7 @@ class UnitTests(unittest.TestCase):
 
     def test_player_vs_abbey(self):
         print("Testing game against abbey...")
-        actual = play(player, abbey, 1000) >= 60
+        actual = play(player, abbey, 1100) >= 60
         self.assertTrue(
             actual,
             'Expected player to defeat abbey at least 60% of the time.')
